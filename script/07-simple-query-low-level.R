6b596bd113cb26207c50eaf50267bf0385e6acc7
Kirill Müller <krlmlr+r@mailbox.org>
2021-12-16 11:48:18 +0100

    When are column types available?

--- script/07-simple-query-low-level.R
+++ script/07-simple-query-low-level.R
@@ -8,14 +8,17 @@ conn <- dbi_connect(context)
 #
 # - receive SQL
 # - send SQL to database
+# - result column names and types are available at the client
 # - fetch results in a loop (up to `n` records)
 # - return results to caller
 
 # ## dbi3 with partitioned streaming
 #
-# - receive SQL, partitioning
-#     - requirement: results are sorted by partitioning
+# - receive SQL, partitioning/chunking
+#     - requirement for partitioning: SQL includes `ORDER BY partitioning`
 # - send SQL to database, receive callback function
+#     - callback for column names and types (called once)
+#     - callback for data (called multiple times)
 # - fetch results in a loop
 #     - callback when partition is complete
 
@@ -30,14 +33,35 @@ conn <- dbi_connect(context)
 # - client retrieves results
 
 qry <- dbi_query(conn, "SELECT 42 AS a", partition = ...)
+# qry <- dbi_query(conn, "SELECT 42 AS a", chunk_size = ...)
 # immediate return, nothing happens on the database
 
 callback <- function(data) {
   print(data$a)
 }
 
-promise <- dbi_send_query(qry, callback)
+promise <- dbi_send_query_callback(qry, callback)
 # query is sent to the database (if possible: async, non-blocking)
 
 promises::promise_all(promise)
+#> [1] 42
+
+# with advance notification of output schema:
+
+promise <- dbi_send_query_callback(qry, callback, notify_schema = TRUE)
+# query is sent to the database (if possible: async, non-blocking)
+
+promises::promise_all(promise)
+#> integer(0)
+#> [1] 42
+
+# Simpler interface, provided by dbi3:
+
+result <- dbi_send_query(qry)
+# query is sent to the database
+
+while (!dbi_has_completed(result)) {
+  print(result$a)
+  result <- dbi_fetch(result)
+}
 #> [1] 42
